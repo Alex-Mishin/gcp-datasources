b61bf1739d503b9a6636162b50e6c8a4815beef3
Alex Mishin <mishin1990@gmail.com>
2020-02-11 18:18:33 +0200

    added .json for files and some new lines

--- cloud_functions/extract_and_store.py
+++ cloud_functions/extract_and_store.py
@@ -5,6 +5,7 @@ from google.cloud import storage
 
 BUCKET = 'api_data_extracted'
 
+
 def extract_url_store_json(event, context):
     """
     Triggered from a message on a Cloud Pub/Sub topic:
@@ -23,6 +24,6 @@ def extract_url_store_json(event, context):
     client = storage.Client()
     bucket = client.bucket(BUCKET)
     url_parts = pubsub_message.split('/')
-    blob_name = '/'.join(url_parts[2:]) + '/' + context.timestamp
+    blob_name = '/'.join(url_parts[2:]) + '/' + context.timestamp + '.json'
     blob = bucket.blob(blob_name)
     blob.upload_from_string(out_str)
