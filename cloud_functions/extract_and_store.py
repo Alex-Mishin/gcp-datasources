69a9bf296226f75d8ddf90920ad818868e2d4833
Alex Mishin <mishin1990@gmail.com>
2020-02-11 17:28:53 +0200

    Added missing functionality for creating new buckets when they don't exist

--- cloud_functions/extract_and_store.py
+++ cloud_functions/extract_and_store.py
@@ -18,9 +18,13 @@ def extract_url_store_json(event, context):
     req = http.request('GET', pubsub_message)
     out_str = req.data.decode('utf-8')
 
-    storage_client = storage.Client()
+    client = storage.Client()
     url_parts = pubsub_message.split('/')
-    bucket = storage_client.bucket(url_parts[2])
+    bucket_name = url_parts[2]
+    if not client.lookup_bucket(bucket_name):
+        client.create_bucket(bucket_name)
+    bucket = client.bucket(bucket_name)
+
     blob_name = '/'.join(url_parts[3:]) + '/' + context.timestamp
     blob = bucket.blob(blob_name)
     blob.upload_from_string(out_str)
