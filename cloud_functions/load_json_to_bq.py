7c213f3092b3f46e4e8a7966e67412c66ce8c4d3
Alex Mishin <mishin1990@gmail.com>
2020-02-12 02:04:15 +0200

    working on load_json_to_bq.py

--- cloud_functions/load_json_to_bq.py
+++ cloud_functions/load_json_to_bq.py
@@ -1,20 +1,26 @@
 from google.cloud import bigquery
 
+PROJECT_ID = 'cypherkin'
+DATASET = 'staging_data'
+
 
 def load_json_to_bq(event, context):
     """
     :param event: (dict): Event payload
     :param context: (google.cloud.functions.Context): Metadata for the event.
     """
-    client = bigquery.Client('cypherkin')
-    dataset_ref = bigquery.dataset.DatasetReference('cypherkin', 'staging_data')
+    client = bigquery.Client(PROJECT_ID)
+    dataset_ref = bigquery.dataset.DatasetReference(PROJECT_ID, DATASET)
+
+    table_name = '_'.join(event['name'].split('/')[:-1]).replace('.', '_')
+    full_name = PROJECT_ID + '.' + DATASET + '.' + table_name
+    client.create_table(full_name, exists_ok=True)
 
     job_config = bigquery.LoadJobConfig()
     job_config.autodetect = True
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
 
     uri = 'gs://' + event['bucket'] + '/' + event['name']
-    table_name = '_'.join(event['name'].split('/')[:-1]).replace('.', '_')
     load_job = client.load_table_from_uri(
         uri, dataset_ref.table(table_name), job_config=job_config
     )
